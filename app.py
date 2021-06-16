ad5e3710a78219428a57100effa767814b2fd475
umutaev <M8858110177@yandex.com>
2021-06-16 09:33:07 +0500

    Refactoring + removed unused dependencies.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from aiohttp import web
-from routes import routes
+
+from .routes import routes
 
 app = web.Application()
 app.add_routes(routes)
--- db.py
+++ db.py
@@ -4,16 +4,18 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
-class TimeRecord(Base):
-	__tablename__ = "timetable"
-	id = Column(Integer, primary_key=True)
-	floor = Column(Integer)
-	record_time = Column(String)
-	username = Column(String)
-	username_second = Column(String)
-	gender = Column(Integer)
 
-engine = create_engine('sqlite:///timetable.db')
+class TimeRecord(Base):
+    __tablename__ = "timetable"
+    id = Column(Integer, primary_key=True)
+    floor = Column(Integer)
+    record_time = Column(String)
+    username = Column(String)
+    username_second = Column(String)
+    gender = Column(Integer)
+
+
+engine = create_engine("sqlite:///timetable.db")
 Base.metadata.bind = engine
 Base.metadata.create_all(engine)
 
