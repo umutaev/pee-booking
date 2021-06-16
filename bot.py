8a64742de861adb58ab1328dea7d4754a0220f96
umutaev <M8858110177@yandex.com>
2021-06-16 10:56:13 +0500

    Aiogram transferring

--- bot.py
+++ bot.py
@@ -1,34 +1,64 @@
-import telebot, requests
+import logging
+from os import environ
 
-token_file = open("token", "r")
-token = token_file.readline().replace("\n", "")
-bot = telebot.TeleBot(token)
+from aiogram import Bot, Dispatcher
+import aiogram
 
-@bot.message_handler(commands=["start"])
-def handle_start(message):
-	markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
-	markup.add("Хочу список команд!🙈")
-	msg = bot.reply_to(message, f'Вечер в хату, @{message.from_user.username}.\nЭтот бот поможет вам онлайн встать в очередь в душ, чтобы все успели пойти под струю мыться, ваши соседи не орали как Братишка, а комната не превратилась в гаупвахту.\nА то Арсений Воронин курлыкать начнёт…\nНаберите /help для просмотра списка команд', reply_markup=markup)
-	bot.register_next_step_handler(msg, process_step)
+# Logging
+logging.basicConfig(filename="bot.log", filemode="a", encoding="utf-8")
 
-@bot.message_handler(commands=["help"])
-def handle_help(message):
-	markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
-	markup.add("Записаться в душ 🚿", "Выйти из очереди🤢", "Посмотреть расписание⌚",
-	           "Посмотреть список команд!🙈")
-	msg = bot.reply_to(message, f'Доступны следующие команды:\n/help - показ этой помощи\n/sign - записаться в очередь под струю\n/delete - выписаться из очереди под струю (только это, ты всё равно помойся, окей?)\n/timetable - посмотреть расписание душа', reply_markup=markup)
-	bot.register_next_step_handler(msg, process_step)
+API_TOKEN = environ["PEE_BOOKING_BOT_TOKEN"]
 
-@bot.message_handler(commands=["/timetable"])
+# Bot
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def handle_start(message: aiogram.types.Message):
+    keyboard_markup = aiogram.types.ReplyKeyboardMarkup(
+        row_width=1, one_time_keyboard=True
+    )
+    keyboard_markup.row(aiogram.types.KeyboardButton("Хочу список команд!🙈"))
+    await message.reply(
+        f"Вечер в хату, @{message.chat.username}.\nЭтот бот поможет вам онлайн "
+        "встать в очередь в душ, чтобы все успели пойти под струю мыться, ваши "
+        "соседи не орали как Братишка, а комната не превратилась в гаупвахту.\n"
+        "А то Арсений Воронин курлыкать начнёт…\nНаберите /help для просмотра"
+        "списка команд",
+        reply_markup=keyboard_markup,
+    )
+
+
+@dp.message_handler(commands=["help"])
+@dp.message_handler(text=["Посмотреть список команд!🙈", "Хочу список команд!🙈"])
+async def handle_help(message: aiogram.types.Message):
+    keyboard_markup = aiogram.types.ReplyKeyboardMarkup(
+        row_width=1, one_time_keyboard=True
+    )
+    buttons = (
+        "Записаться в душ 🚿",
+        "Выйти из очереди🤢",
+        "Посмотреть расписание⌚",
+        "Посмотреть список команд!🙈",
+    )
+    keyboard_markup.add(*(aiogram.types.KeyboardButton(text) for text in buttons))
+    await message.reply(
+        f"Доступны следующие команды:\n"
+        "/help - показ этой помощи\n"
+        "/sign - записаться в очередь <s>на приём к Арсению Воронину</s> помыться\n"
+        "/delete - выписаться из очереди под струю (только это, ты всё равно помойся, окей?)\n"
+        "/timetable - посмотреть расписание душа",
+        reply_markup=keyboard_markup,
+        parse_mode=aiogram.types.ParseMode.HTML,
+    )
+
+
+@dp.message_handler(commands=["/timetable"])
 def handle_timetable(message):
-	markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
-	markup.add("Записаться в душ 🚿", "Выйти из очереди🤢", "Посмотреть расписание⌚",
-	           "Посмотреть список команд!🙈")
-	msg = bot.reply_to(message, f'', reply_markup=markup)
-	bot.register_next_step_handler(msg, process_step)
+    ...
+    # TODO: shower timetable
 
-def process_step(message):
-	if message.text == "Хочу список команд!🙈" or message.text == "Посмотреть список команд!🙈":
-		handle_help(message)
 
-bot.polling(none_stop=True)
+if __name__ == "__main__":
+    aiogram.utils.executor.start_polling(dp)
